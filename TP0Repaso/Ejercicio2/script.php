76d70a338fd42024de7ba7783bfacd5182377fe8
gonzalo <gonzaloolmos1998@gmail.com>
2022-08-29 10:29:39 -0300

    se agregan trabajos

---
+++ TP0Repaso/Ejercicio2/script.php
@@ -0,0 +1,9 @@
+<?php
+
+$nombre = "Gonzalo";
+$apellido = "Olmos";
+$edad = "23";
+$direccion = "Villa Regina";
+
+echo strtoupper($nombre)." ".strtoupper($apellido)." ".strtoupper($edad)." ".strtoupper($direccion);
+
---
+++ TP0Repaso/Ejercicio3/script.php
@@ -0,0 +1,12 @@
+<?php
+
+$nombre = "Gonzalo ";
+$apellido = " Olmos";
+$edad = " 23 ";
+$direccion = " Villa Regina";
+
+$array= array($nombre, $apellido, $edad, $direccion); 
+
+echo "hola <br>"; 
+echo implode($array);
+
